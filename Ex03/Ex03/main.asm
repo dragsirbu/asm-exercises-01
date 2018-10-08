3a566aca3db775cfc37e261873ed9b6ce7d42a98
Dragos Sirbu <dragsirbu@gmail.com>
2018-10-08 11:39:22 +0200

    Finished ex3 and ex4

---
+++ Ex03/Ex03/main.asm
@@ -0,0 +1,15 @@
+;
+; Ex03.asm
+;
+; Created: 08-Oct-18 10:36:06 AM
+; Author : drags
+;
+
+
+; Replace with your application code
+
+	ldi	r16, 42
+	ldi r17, 0x21
+	ldi r18, 0b10110010
+	nop;
+
---
+++ Ex04/Ex04/main.asm
@@ -0,0 +1,17 @@
+;
+; Ex04.asm
+;
+; Created: 08-Oct-18 10:44:26 AM
+; Author : drags
+;
+
+
+; Replace with your application code
+
+	ldi r17, 0x08; Load value 0x08 to register r17
+	ldi r18, 0xb3; Load value 0xb3 into register r18
+	add r18, r17; Add r17 to r18 (r18=r18+r17)
+	nop
+
+
+
